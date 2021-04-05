da286d40e70d2f01ece3783091147af466fc1d8e
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-05 22:20:04 +0530

    selection_sort

---
+++ Sorting Algo/selection_sort.py
@@ -0,0 +1,19 @@
+#Best case, worst O(n^2)
+print("Selection sort")
+print("__"*30)
+arr = [3, 6, 4, 1, 7, 9, 2]
+n=len(arr)
+
+for i in range(0, n-1):
+    smallest=i
+
+    #Finding smallest value for between all the values to right
+    for j in range(i+1, n):
+        if arr[j] < arr[smallest]:
+            smallest = j
+
+    #Swap current index with new smalles value found
+    arr[i], arr[smallest] = arr[smallest], arr[i]
+
+
+print(arr)

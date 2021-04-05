f352b54f8efb88a7cab327b79392d44825bf58eb
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-05 20:24:32 +0530

    .

--- Sorting Algo/insertion_sort.py
+++ Sorting Algo/insertion_sort.py
@@ -1,7 +1,9 @@
-print("Insertion sort")
-print("__" * 30)
 #Best case O(n)
 #Worst case O(n^2)
+#Use when only few items or items are mostly sorted already
+print("Insertion sort")
+print("__" * 30)
+
 arr = [3, 6, 4, 1, 7, 9, 2]
 n = len(arr)
 
@@ -9,12 +11,12 @@ n = len(arr)
 def insert(pos, val):
     print("For value: ", val)
     prev= pos-1
-    new_pos= pos
+
     while(prev>=0 and arr[prev] > val):
-        arr[prev], arr[new_pos]= arr[new_pos], arr[prev]
-        print("Swapping",prev,new_pos)
-        prev-=1
-        new_pos-=1
+        arr[prev], arr[prev+1]= arr[prev+1], arr[prev]
+        print("Swapping", prev, prev+1)
+        prev -= 1
+
 
 
 for i in range(1, n):

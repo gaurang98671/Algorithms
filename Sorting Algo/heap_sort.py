75c005a1ffb26ea5393b091f633891df9f4154e2
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-07 02:08:52 +0530

    sorted max heap

--- Sorting Algo/heap_sort.py
+++ Sorting Algo/heap_sort.py
@@ -1,17 +1,16 @@
 arr = [1, 12, 9, 5, 6, 10]
 n = len(arr)
-
+new_arr=[]
 
 def heapify(i):
     largest = i
     left = 2 * i + 1
     right = 2 * i + 2
 
-    if left < n \
-            and arr[left] > arr[largest]:
+    if left < len(arr) and arr[left] > arr[largest]:
         largest = left
 
-    if right < n and arr[right] > arr[largest]:
+    if right < len(arr) and arr[right] > arr[largest]:
         largest = right
 
     # swap root with largest
@@ -27,3 +26,15 @@ for i in range((n//2)-1, -1, -1):
     print("For:  ", i)
     heapify(i)
     print(arr)
+
+while(len(arr)!=1):
+    arr[0], arr[len(arr)-1]= arr[len(arr)-1], arr[0]
+    print("After swapping", arr)
+    print("Popping", arr[len(arr)-1])
+    new_arr.append(arr[len(arr)-1])
+    arr.pop(len(arr)-1)
+    print("after popping", arr)
+
+    heapify(0)
+
+print(new_arr)

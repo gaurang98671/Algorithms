b1ada294e3dc019df9a9e288f31c3e897a2f38f4
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-13 20:34:22 +0530

    merge sort

---
+++ Sorting Algo/merge_sort.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+
+        #find mid
+        mid = len(arr)//2
+
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+                k += 1
+
+            else:
+                arr[k] = right[j]
+                j += 1
+                k += 1
+
+        #Check for remaining elements
+        #For left
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        #For right
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+a= [5,4,3,2,1]
+
+merge_sort(a)
+print(a)

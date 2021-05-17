a07ea79200862c2e22d73b2fcd2225e0ebdf4fb1
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-05-18 04:59:40 +0530

    Update Graph.py

--- Data structures/Graph.py
+++ Data structures/Graph.py
@@ -7,7 +7,27 @@ class Graph:
                 self.graph_dict[start].append(end)
             else:
                 self.graph_dict[start] = [end]
-        
+
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start is end:
+            return path
+
+        if start not in self.graph_dict:
+            return []
+
+        paths =[]
+        for node in self.graph_dict[start]:
+
+            if node not in path:
+                new_path = self.get_paths(node, end, path)
+
+                for p in new_path:
+                    paths.append(p)
+        return paths
+
+
 if __name__ == '__main__':
 
     routes = [
@@ -20,3 +40,6 @@ if __name__ == '__main__':
     ]
 
     route_graph = Graph(routes)
+
+    print(route_graph.get_paths('Mumbai', 'New York'))
+    print(route_graph.graph_dict)

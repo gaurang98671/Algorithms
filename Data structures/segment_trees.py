f1a440160b96e3bbee14ebf4e185fbb12d37026f
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-07-06 23:44:49 +0530

    self attempt

--- Data structures/segment_trees.py
+++ Data structures/segment_trees.py
@@ -1,15 +1,23 @@
 arr = [1, 2, 3, 4, 5]
-segment_tree = []
+segment_tree = [0 for x in range(10)]
 
-def create_tree(arr):
-    print(arr)
-    if len(arr)==1:
+
+def create_tree(arr, parent, node_type=None):
+    if node_type == 'l':
+        segment_tree[(2*parent)+1] = sum(arr)
+    if node_type == 'r':
+        segment_tree[(2*parent)+2] = sum(arr)
+    if parent == -1:
+        segment_tree[0] = sum(arr)
+
+    if len(arr) == 1:
         return
     else:
         mid = len(arr)//2
         left = arr[:mid]
         right = arr[mid:]
 
-        create_tree(left)
-        create_tree(right)
-create_tree(arr)
+        create_tree(left, parent+1,'l')
+        create_tree(right, parent+1, 'r')
+create_tree(arr, -1, None)
+print(segment_tree)
